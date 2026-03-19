810011bca97e93b6698b7687a2c965271616eec9
bensandom <bensandom@gmail.com>
2026-03-19 21:08:56 +0000

    Increase username and password field lengths

--- Change_Demo_System/models.py
+++ Change_Demo_System/models.py
@@ -3,8 +3,8 @@ from flask_login import UserMixin
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(100), unique =True, nullable = False)
-    password = db.Column(db.String(20), nullable = False)
+    username = db.Column(db.String(120), unique =True, nullable = False)
+    password = db.Column(db.String(50), nullable = False)
     role = db.Column(db.String(20), nullable=False, default='User')
 
     groups = db.relationship('Group', secondary='user_group', backref='users')
