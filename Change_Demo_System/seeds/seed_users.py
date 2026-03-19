1b25bd9d8c8d6bdec6730076c54716db13f71d1a
bensandom <bensandom@gmail.com>
2026-03-19 21:23:48 +0000

    Fix user passwords in seed_users function
    Updated user passwords in seed_users function to match usernames.

--- Change_Demo_System/seeds/seed_users.py
+++ Change_Demo_System/seeds/seed_users.py
@@ -2,14 +2,14 @@ from Change_Demo_System.extensions import db
 from Change_Demo_System.models import User
 
 def seed_users():
-    service_desk_approver = User(username="sd_approver", password="service_desk_approver", role="Admin")
-    service_desk_user = User(username="sd_user", password="service_desk_user", role="User")
-    request_team_approver = User(username="req_approver", password="request_approver", role="Admin")
-    request_team_user = User(username="req_user", password="request_user", role="User")
-    infrastructure_team_approver = User(username="inf_approver", password="infrastructure_approver", role="Admin")
-    infrastructure_team_user = User(username="inf_user", password="infrastructure_user", role="User")
-    application_support_approver = User(username="app_sup_approver", password="application_support_approver", role="Admin")
-    application_support_user = User(username="app_sup_user", password="application_support_user", role="User")
+    service_desk_approver = User(username="sd_approver", password="sd_approver", role="Admin")
+    service_desk_user = User(username="sd_user", password="sd_user", role="User")
+    request_team_approver = User(username="req_approver", password="req_approver", role="Admin")
+    request_team_user = User(username="req_user", password="req_user", role="User")
+    infrastructure_team_approver = User(username="inf_approver", password="inf_approver", role="Admin")
+    infrastructure_team_user = User(username="inf_user", password="inf_user", role="User")
+    application_support_approver = User(username="app_sup_approver", password="app_sup_approver", role="Admin")
+    application_support_user = User(username="app_sup_user", password="app_sup_user", role="User")
     default_admin = User(username="admin", password="admin", role="Admin")
     change_manager = User(username="Change_Manager", password="change_manager", role="Admin")
     default_user = User(username="user", password="user", role="User")
